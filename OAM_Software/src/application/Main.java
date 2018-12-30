163fb738ef511697c650ac97229bdc7ea42d85ba
LBeast13 <liam.bette@hotmail.com>
2018-12-30 11:26:25 +0100

    Init Stage in Main

--- OAM_Software/src/application/Main.java
+++ OAM_Software/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,23 +11,82 @@ import java.time.LocalDate;
 import dao.factory.AbstractDAOFactory;
 import dao.model.DAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import model.Member;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private Stage mainStage;
+	private BorderPane mainContainer;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		mainStage = primaryStage;
+		mainStage.setTitle("Olympic Art Malaga");
+		mainStage.setMaximized(true);
+		
+		initializeMainContainer();
+		initializeHomePage();
+	}
+	
+	/**
+	 * Initializes the main container of the software.
+	 */
+	private void initializeMainContainer() {
+		
+		FXMLLoader loader = new FXMLLoader();
+		
+		// We specify the relative path of the view to the loader
+		loader.setLocation(Main.class.getResource("..\\view\\MainContainerView.fxml"));
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
+			// The loading gives us the container
+			mainContainer = (BorderPane) loader.load();
 			
+			// Set a primary scene with our container
+			Scene scene = new Scene(mainContainer);
+			
+			// Set the style for the Software
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// We set the scene of our main stage
+			mainStage.setScene(scene);
+			
+			// Displays the stage
+			mainStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Initializes and displays the Home Page
+	 */
+	private void initializeHomePage() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("..\\view\\HomePageView.fxml"));
+		try {
+			// Container loading
+			BorderPane conteneurPersonne = (BorderPane) loader.load();
+			
+			// We set it to the center of the main container
+			mainContainer.setCenter(conteneurPersonne);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+		
+		/* Temp testing code for Database access
+		try {
+		
 			AbstractDAOFactory adf = AbstractDAOFactory.getFactory(AbstractDAOFactory.DB_DAO_FACTORY);
 			//On récupère un objet faisant le lien entre la base et nos objets 
 			DAO<Member> eleveDao = adf.getMemberDAO();
@@ -51,10 +111,6 @@ public class Main extends Application {
 
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
+		}*/
 	}
 }
